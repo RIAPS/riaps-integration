057929329536f0cebf1b17dd539e744c6ba11a25
Mary Metelko <mary@metelkos-MacBook-Pro.local>
2017-04-19 15:40:41 -0500

    Script working, need to do final testing on base, then integrate with rest of integration scripts

--- scripts/bbb/riaps_bbb_base_setup.sh
+++ scripts/bbb/riaps_bbb_base_setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
 RIAPSAPPDEVELOPER=riaps
 sethostname=bin/set_unique_hostname.py
 sethostservice=conf/sethostname.service
-gpiorule=etc_udev/80-non-root-gpio-permissions.rules
-
+gpiorule_dir=etc_udev/
+gpiorule=80-non-root-gpio-permissions.rules
+kernelupdate=/opt/scripts/tools/update_kernel.sh
 
 
 if [ -f "../setup.conf" ]
@@ -16,9 +18,10 @@ else
 fi
 source ../setup.conf
 
+
 # Install RT Kernel
 rt_kernel_install() {
-	sudo ./opt/scripts/tools/update_kernel.sh --ti-rt-kernel --lts-4_9
+	sudo $kernelupdate --ti-rt-kernel --lts-4_9
 }
 
 # Set hostname to bbb-xxxx, where xxxx is the last 4 digits of the MAC address
@@ -33,28 +36,30 @@ hostname_setup() {
 # Setup user account
 user_func() {	 
 	sudo useradd -m -c "RIAPS App Developer" $RIAPSAPPDEVELOPER -s /bin/bash -d /home/$RIAPSAPPDEVELOPER
-	sudo echo -e "riaps\nriaps" | sudo passwd $RIAPSAPPDEVELOPER
+	sudo echo -e "riapspwd\nriapspwd" | sudo passwd $RIAPSAPPDEVELOPER
 	 
 	sudo usermod -aG sudo $RIAPSAPPDEVELOPER
 }
 
 # Configure the login information
-#splash_screen_update() {
+splash_screen_update() {
 # MM TODO:  add splash screen change with RIAPS disclaimer and login info change
-#}
+	echo "Splash screen update is TBD"
+}
 
 # Install ssh keys from setup.conf
 install_riaps_keys() {
 	sudo -H -u $1 mkdir -p /home/$1/.ssh
 	sudo -H -u $1 touch /home/$1/.ssh/authorized_keys
+	sudo -H -u $1 chmod 777 /home/$1/.ssh/authorized_keys
 	sudo -H -u $1 cat ../$SSH_PUBLIC_KEY >> /home/$1/.ssh/authorized_keys	
-	sudo -H -u $1 cp ../$SSH_PUBLIC_KEY /home/$1/.ssh/
+ 	sudo -H -u $1 cp ../$SSH_PUBLIC_KEY /home/$1/.ssh/
 	sudo -H -u $1 chmod 600 /home/$1/.ssh/authorized_keys  
-	sudo -H -u $1 touch /home/$1/.ssh/id_rsa
-	sudo -H -u $1 cp ../$SSH_PRIVATE_KEY /home/$1/.ssh
+	sudo -H -u $1 cp ../$SSH_PRIVATE_KEY /home/$1/.ssh/$SSH_PRIVATE_KEY
 	sudo -H -u $1 chmod 600 /home/$1/.ssh/$SSH_PRIVATE_KEY	 
-	sudo -H -u $1 ssh-agent /bin/bash
-	sudo -H -u $1 ssh-add /home/$1/.ssh/$SSH_PRIVATE_KEY
+# Note:  This was used in the manual build of the BBB image, but purposely leaving it out now
+#	sudo -H -u $1 ssh-agent /bin/bash
+#	sudo -H -u $1 ssh-add /home/$1/.ssh/$SSH_PRIVATE_KEY
 }
 
 # Setup any system utilities
@@ -68,13 +73,13 @@ utilities_setup() {
 # Install Random Number Generator used by RIAPS Discovery Service
 randomnum_install() {
 	sudo apt-get install rng-tools -y
-	sudo systemctl enable rng-tools.service	
+	sudo systemctl start rng-tools.service	
 }
 
 # Turn off Frequency Governing
 freqgov_off() {
 	sudo touch /etc/default/cpufrequtils
-	sudo cat 'GOVERNOR="performance"' > /etc/default/cpufrequtils
+	echo "GOVERNOR=\"performance\"" | sudo tee -a /etc/default/cpufrequtils
 	sudo update-rc.d ondemand disable
 	sudo /etc/init.d/cpufrequtils restart
 }
@@ -87,22 +92,23 @@ hwconfig_setup() {
 
 	if [$SLOTS eq ""]
 	then
-		sudo -H -u $1 echo "export SLOTS=/sys/devices/platform/bone_capemgr/slots" >> /home/$1/.bashrc  
+		echo -en "\n# RIAPS Device Slots\nexport SLOTS=/sys/devices/platform/bone_capemgr/slots\n" | sudo tee -a /home/riaps/.bashrc
 	fi
 		
 	if [$PINS eq ""]
 	then
-		sudo -H -u $1 echo "export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins" >> /home/$1/.bashrc  
+		echo -en "\n# RIAPS Device Pins\nexport PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins\n" | sudo tee -a /home/riaps/.bashrc  
 	fi
 		
 	# add 'riaps' to device output groups
 	sudo groupadd gpio
-	sudo -H -u $1 usermod -a -G dialout $1
-	sudo -H -u $1 usermod -a -G gpio $1
-	sudo cp $gpiorule /etc/udev/rules.d
-	sudo chmod 744 /etc/udev/rules.d/$gpiorule
+	sudo usermod -aG dialout $1
+	sudo usermod -aG gpio $1
+	sudo cp $gpiorule_dir$gpiorule /etc/udev/rules.d
+	sudo chmod 644 /etc/udev/rules.d/$gpiorule
 	
 	# Update visudo to retain the environment variables on a su call
+	sudo sh -c "echo -e \"\n# Retain Device Environment Variables (RIAPS)\" >> /etc/sudoers"
 	sudo sh -c "echo \"Defaults    env_keep += \"SLOTS\"\" >> /etc/sudoers"
 	sudo sh -c "echo \"Defaults    env_keep += \"PINS\"\"  >> /etc/sudoers"
 	
@@ -112,20 +118,29 @@ hwconfig_setup() {
 
 # Remove unnecessary packages that may interfere
 unneeded_removal() {
-	sudo apt-get remove connman
+# MM TODO:  is this needed?	sudo apt-get remove connman -y
+	echo "Nothing to remove at this time"
 }
 
 # RIAPS Specified Middleware
 middleware_install() {
 	sudo apt-get install pps-tools linuxptp libnss-mdns gpsd gpsd-clients chrony -y 
 	sudo apt-get install libcapnp-dev libssl-dev libffi-dev -y
+    sudo pip3 install 'redis>=2.10.5' 'hiredis >= 0.2.0'  # expect to remove soon (MM)
     sudo pip3 install 'pyzmq>=16' 'textX>=1.4' 'pycapnp >= 0.5.9' 'netifaces>=0.10.5' 'paramiko>=2.0.2' 'cryptography>=1.5.3'
-    sudo pip3 install git+https://github.com/adubey14/rpyc #egg=rpyc-3.3.1
+    sudo -H pip3 install git+https://github.com/adubey14/rpyc #egg=rpyc-3.3.1
 }
 
 # Install RIAPS deb packages
 riapsdeb_install() {
-	sudo ../install_integration.sh
+	echo "Stopped here to check progress"
+#	sudo ../install_integration.sh
+}
+
+# Cleanup after installation
+remove_installartifacts() {
+	rm -r scripts
+	rm scripts.tar.gz
 }
 
 
@@ -135,8 +150,8 @@ hostname_setup
 echo "BBB hostname configured"
 user_func 
 echo "Created riaps user account"
-#splash_screen_update
-#echo "Splash screen updated"
+splash_screen_update
+echo "Splash screen updated"
 install_riaps_keys $RIAPSAPPDEVELOPER
 echo "Installed riaps user key"
 utilities_setup
@@ -144,7 +159,7 @@ echo "System utilities setup"
 randomnum_install
 echo "Random number generator installed"
 freqgov_off
-echo "Turn off frequency governing"
+echo "Frequency governing is turned off"
 hwconfig_setup $RIAPSAPPDEVELOPER
 echo "HW device specific configurations done"
 unneeded_removal
@@ -153,3 +168,5 @@ middleware_install
 echo "Installed RIAPS required middleware"
 riapsdeb_install
 echo "RIAPS deb packages installed"
+remove_installartifacts
+echo "Cleanup after installation done"
