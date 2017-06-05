18135c4a23dc22d4d99de1a28798c587227e7935
Mary Metelko <MMetelko@users.noreply.github.com>
2017-06-05 17:55:57 -0500

    Moved to new pycom with textX at version 1.4

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 export externalsversion="0.4.4" 
 export coreversion="0.4.5"
-export pycomversion="0.4.5"
+export pycomversion="0.4.6"
 export timesyncversion="1.1.1"
 export riapsdsmlversion="0.4.1"
 
