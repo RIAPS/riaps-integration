1141ffd8ea401751cb7ce4295e6564b1c9292bce
Mary Metelko <MMetelko@users.noreply.github.com>
2017-06-20 10:47:45 -0500

    Update version.sh

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 export externalsversion="0.4.4" 
 export coreversion="0.4.5"
-export pycomversion="0.4.8"
+export pycomversion="0.4.10"
 export timesyncversion="1.1.1"
 export riapsdsmlversion="0.4.1"
 
