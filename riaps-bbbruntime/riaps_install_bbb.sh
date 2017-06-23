b0636415ccfc15a01ad903fa16f9602c9fa160c5
MMetelko <MMetelko@users.noreply.github.com>
2017-06-23 13:35:50 -0500

    Removing previous directory structure

--- riaps-bbbruntime/riaps_install_bbb.sh
+++ riaps-bbbruntime/riaps_install_bbb.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e 
 
-sudo apt-get install riaps-release/riaps-externals-armhf.deb
+sudo apt-get install riaps-externals-armhf.deb
 echo "installed externals"
-sudo apt-get install riaps-release/riaps-core-armhf.deb
+sudo apt-get install riaps-core-armhf.deb
 echo "installed core"
-sudo apt-get install riaps-release/riaps-pycom-armhf.deb
+sudo apt-get install riaps-pycom-armhf.deb
 echo "installed pycom"
-sudo apt-get install riaps-release/riaps-systemd-armhf.deb 
+sudo apt-get install riaps-systemd-armhf.deb 
 echo "installed services"
-sudo apt-get install riaps-release/riaps-timesync-armhf.deb 
+sudo apt-get install riaps-timesync-armhf.deb 
 echo "installed timesync"
 echo "installed RIAPS platform"
--- riaps-x86runtime/riaps_install_amd64.sh
+++ riaps-x86runtime/riaps_install_amd64.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e 
 
-sudo apt-get install riaps-release/riaps-externals-amd64.deb
+sudo apt-get install riaps-externals-amd64.deb
 echo "installed externals"
-sudo apt-get install riaps-release/riaps-core-amd64.deb
+sudo apt-get install riaps-core-amd64.deb
 echo "installed core"
-sudo apt-get install riaps-release/riaps-pycom-amd64.deb
+sudo apt-get install riaps-pycom-amd64.deb
 echo "installed pycom"
-sudo apt-get install riaps-release/riaps-systemd-amd64.deb 
+sudo apt-get install riaps-systemd-amd64.deb 
 echo "installed services"
-sudo apt-get install riaps-release/riaps-timesync-amd64.deb 
+sudo apt-get install riaps-timesync-amd64.deb 
 echo "installed timesync"
 echo "installed RIAPS platform"
