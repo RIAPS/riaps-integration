3cf316cf636cabc53a24dab7a9acafdedcb13acc
Abhishek Dubey <abhishek.dubey@vanderbilt.edu>
2017-07-07 11:13:02 -0500

    Bumping core to 0.4.8
    armhf header inclusion

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 export externalsversion="0.4.4" 
-export coreversion="0.4.7"
+export coreversion="0.4.8"
 export pycomversion="0.4.12"
 export timesyncversion="1.1.2"
 export riapsdsmlversion="0.4.1"
