6ae2890c7f9664cecfac7f889421a923d6635b30
Mary Metelko <MMetelko@users.noreply.github.com>
2017-07-18 14:18:51 -0500

    Extra update never hurts

--- riaps-bbbruntime/riaps_install_bbb.sh
+++ riaps-bbbruntime/riaps_install_bbb.sh
@@ -4,5 +4,6 @@ set -e
 sudo apt-get install rdate
 sudo rdate -n -4 time-a.nist.gov
 
+sudo apt-get update
 sudo apt-get install riaps-externals-armhf riaps-core-armhf riaps-pycom-armhf riaps-systemd-armhf riaps-timesync-armhf -y
 echo "installed RIAPS platform"
