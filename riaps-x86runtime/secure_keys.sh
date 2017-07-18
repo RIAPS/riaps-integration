407f07f59dc7e4ca9e4b9b5135869958b8d8be0f
Mary Metelko <MMetelko@users.noreply.github.com>
2017-07-18 11:09:49 -0500

    public key name adjust

--- riaps-x86runtime/secure_keys.sh
+++ riaps-x86runtime/secure_keys.sh
@@ -10,4 +10,4 @@ chmod 600 $1
 chmod 600 $2
 scp -i $1 $2 riaps@$4:~/.ssh/id_rsa.key
 scp -i $1 $3 riaps@$4:~/.ssh/id_rsa.pub
-ssh -i $1 -l riaps  $4  'cp ~/.ssh/authorized_keys ~/.ssh/authorized_keys.bak; cp ~/.ssh/id_rsa.key.pub ~/.ssh/authorized_keys'
+ssh -i $1 -l riaps  $4  'cp ~/.ssh/authorized_keys ~/.ssh/authorized_keys.bak; cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys'
