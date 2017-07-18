4b34f9bf7f5385d4b4c2eeb8398b5f427217d251
Mary Metelko <MMetelko@users.noreply.github.com>
2017-07-18 13:56:45 -0500

    Add rdate

--- riaps-x86runtime/riaps_install_amd64.sh
+++ riaps-x86runtime/riaps_install_amd64.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e 
 
+sudo apt-get install rdate
+sudo rdate -n -4 time-a.nist.gov
+
 sudo apt-get install riaps-externals-amd64 riaps-core-amd64 riaps-pycom-amd64 riaps-systemd-amd64 riaps-timesync-amd64 -y
 echo "installed RIAPS platform"
  
