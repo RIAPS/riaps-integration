224911880042cfa62e5dc05d009cb4656b4f534e
Mary Metelko <MMetelko@users.noreply.github.com>
2017-07-18 13:57:15 -0500

    Add rdate

--- riaps-bbbruntime/riaps_install_bbb.sh
+++ riaps-bbbruntime/riaps_install_bbb.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e 
 
+sudo apt-get install rdate
+sudo rdate -n -4 time-a.nist.gov
+
 sudo apt-get install riaps-externals-armhf riaps-core-armhf riaps-pycom-armhf riaps-systemd-armhf riaps-timesync-armhf -y
 echo "installed RIAPS platform"
