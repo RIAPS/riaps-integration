039bebc195b13bf5f4b95380f189f987fe7a5de6
Mary Metelko <MMetelko@users.noreply.github.com>
2017-07-18 14:35:20 -0500

    Bump Core Version

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 export externalsversion="0.4.4" 
-export coreversion="0.4.8"
+export coreversion="0.4.9"
 export pycomversion="0.4.15"
 export timesyncversion="1.1.2"
 export riapsdsmlversion="0.4.1"
