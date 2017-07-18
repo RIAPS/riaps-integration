2085d4795c927aef326a75f6b73f158ee75c72f2
Mary Metelko <MMetelko@users.noreply.github.com>
2017-07-18 14:18:24 -0500

    Extra update never hurts

--- riaps-x86runtime/riaps_install_amd64.sh
+++ riaps-x86runtime/riaps_install_amd64.sh
@@ -4,6 +4,7 @@ set -e
 sudo apt-get install rdate
 sudo rdate -n -4 time-a.nist.gov
 
+sudo apt-get update
 sudo apt-get install riaps-externals-amd64 riaps-core-amd64 riaps-pycom-amd64 riaps-systemd-amd64 riaps-timesync-amd64 -y
 echo "installed RIAPS platform"
  
