1ba20f6ea72482a2f937dda2576012ac2d468c32
Mary Metelko <MMetelko@users.noreply.github.com>
2017-07-24 09:51:07 -0500

    Update version.sh
    Bumping capnp version from v0.5.3 to v0.5.3.1

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
-export externalsversion="0.4.4" 
-export coreversion="0.4.9"
+export externalsversion="0.4.8" 
+export coreversion="0.5.0"
 export pycomversion="0.4.17"
 export timesyncversion="1.1.2"
 export riapsdsmlversion="0.4.1"
