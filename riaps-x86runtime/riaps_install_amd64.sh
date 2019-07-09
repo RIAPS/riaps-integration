4d6e47962ece289f54ce75c864593841c66ae46c
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2019-07-09 13:46:23 -0500

    Update install script to remove riaps-systemd and completely uninstall and reinstall riaps-pycom and riaps-timesync

--- riaps-x86runtime/riaps_install_amd64.sh
+++ riaps-x86runtime/riaps_install_amd64.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
-set -e 
+set -e
 
 # make sure date is correct
 sudo apt-get install rdate
 sudo rdate -n -4 time.nist.gov
-    
+
 # make sure pip is up to date
-sudo pip3 install --upgrade pip 
+sudo pip3 install --upgrade pip
+
+# uninstall deprecated package - riaps-systemd-<arch>
+sudo dpkg -r riaps-systemd-amd64 || true
+sudo dpkg --purge riaps-systemd-amd64 || true
+
+# For v1.1.16, riaps-pycom and riaps-timesync uninstall package was modified
+# fully remove packages and then remove extra files that no longer exist.
+# This future releases, this will not be necessary.
+sudo dpkg -r riaps-pycom-amd64 || true
+sudo dpkg -r riaps-timesync-amd64 || true
+sudo dpkg --purge riaps-pycom-amd64 || true
+sudo dpkg --purge riaps-timesync-amd64 || true
+sudo rm -f /etc/profile.d/10-riaps-external-amd64.sh || true
+sudo rm -f /etc/apparmor.d/cache/usr.local.bin.riaps_actor || true
 
 # install RIAPS packages
 sudo apt-get update
-sudo apt-get install riaps-externals-amd64 riaps-core-amd64 riaps-pycom-amd64 riaps-systemd-amd64 riaps-timesync-amd64 -y
+sudo apt-get install riaps-externals-amd64 riaps-core-amd64 riaps-pycom-amd64 riaps-timesync-amd64 -y
 echo "installed RIAPS platform"
-
-
