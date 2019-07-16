a449be865aa81b70812909729dfe0db0883fc57d
Mary Metelko <MMetelko@users.noreply.github.com>
2019-07-16 10:02:19 -0500

    Update install scripts (#57)

--- riaps-bbbruntime/riaps_install_bbb.sh
+++ riaps-bbbruntime/riaps_install_bbb.sh
@@ -8,20 +8,16 @@ sudo rdate -n -4 time.nist.gov
 sudo pip3 install --upgrade pip
 
 # uninstall deprecated package - riaps-systemd-<arch>
-sudo dpkg -r riaps-systemd-armhf || true
-sudo dpkg --purge riaps-systemd-armhf || true
+sudo apt-get purge riaps-systemd-armhf || true
 
 # For v1.1.16, riaps-pycom and riaps-timesync uninstall package was modified
 # fully remove packages and then remove extra files that no longer exist.
 # This future releases, this will not be necessary.
-sudo dpkg -r riaps-pycom-armhf || true
-sudo dpkg -r riaps-timesync-armhf || true
-sudo dpkg --purge riaps-pycom-armhf || true
-sudo dpkg --purge riaps-timesync-armhf || true
+sudo apt-get purge riaps-pycom-armhf || true
+sudo apt-get purge riaps-timesync-armhf || true
 sudo rm -f /etc/profile.d/10-riaps-external-armhf.sh || true
 sudo rm -f /etc/apparmor.d/cache/usr.local.bin.riaps_actor || true
 sudo rm -f /etc/timesync.role || true
-sudo rm -r /usr/local/share/timesync/config/__pycache__ || true
 sudo rm -rf /usr/local/share/timesync || true
 
 # install RIAPS packages
--- riaps-x86runtime/riaps_install_amd64.sh
+++ riaps-x86runtime/riaps_install_amd64.sh
@@ -9,19 +9,17 @@ sudo rdate -n -4 time.nist.gov
 sudo pip3 install --upgrade pip
 
 # uninstall deprecated package - riaps-systemd-<arch>
-sudo dpkg -r riaps-systemd-amd64 || true
-sudo dpkg --purge riaps-systemd-amd64 || true
+sudo apt-get purge riaps-systemd-amd64 || true
 
 # For v1.1.16, riaps-pycom and riaps-timesync uninstall package was modified
 # fully remove packages and then remove extra files that no longer exist.
 # This future releases, this will not be necessary.
-sudo dpkg -r riaps-pycom-amd64 || true
-sudo dpkg -r riaps-timesync-amd64 || true
-sudo dpkg --purge riaps-pycom-amd64 || true
-sudo dpkg --purge riaps-timesync-amd64 || true
+sudo apt-get purge riaps-pycom-amd64 || true
+sudo apt-get spurge riaps-timesync-amd64 || true
 sudo rm -f /etc/profile.d/10-riaps-external-amd64.sh || true
 sudo rm -f /etc/apparmor.d/cache/usr.local.bin.riaps_actor || true
-sudo rm -rf /usr/local/riaps/etc/ || true
+sudo rm -f /etc/timesync.role || true
+sudo rm -rf /usr/local/share/timesync || true
 sudo rm -rf /usr/local/riaps/ || true
 
 # install RIAPS packages
