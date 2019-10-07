49ee9cc09bc64c8695cfdee87f8be63edba6b3ad
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2019-10-07 17:26:01 -0500

    Tabs to spaces

--- riaps-x86runtime/bootstrap.sh
+++ riaps-x86runtime/bootstrap.sh
@@ -165,30 +165,30 @@ nethogs_prereq_install() {
 
 #install other required packages
 other_pip3_installs(){
-	pip3 install 'Adafruit_BBIO == 1.1.1' 'pydevd==1.4.0' 'rpyc==4.1.0' 'redis==2.10.6' 'hiredis == 0.2.0' 'netifaces==0.10.7' 'paramiko==2.6.0' 'cryptography==2.7' 'cgroups==0.1.0' 'cgroupspy==0.1.6' 'psutil==5.4.2' 'butter==0.12.6' 'lmdb==0.94' 'fabric3==1.14.post1' 'pyroute2==0.5.2' 'minimalmodbus==0.7' 'pyserial==3.4' 'pybind11==2.2.4' 'toml==0.10.0' 'pycryptodomex==3.7.3' --verbose
-	pip3 install --ignore-installed 'PyYAML==5.1.1'
-	pip3 install 'textX==1.7.1' 'graphviz==0.5.2' 'pydot==1.2.4' 'gitpython==2.1.11' 'pymultigen==0.2.0' 'Jinja2==2.10' --verbose
-        echo "installed pip3 packages"
+    pip3 install 'Adafruit_BBIO == 1.1.1' 'pydevd==1.4.0' 'rpyc==4.1.0' 'redis==2.10.6' 'hiredis == 0.2.0' 'netifaces==0.10.7' 'paramiko==2.6.0' 'cryptography==2.7' 'cgroups==0.1.0' 'cgroupspy==0.1.6' 'psutil==5.4.2' 'butter==0.12.6' 'lmdb==0.94' 'fabric3==1.14.post1' 'pyroute2==0.5.2' 'minimalmodbus==0.7' 'pyserial==3.4' 'pybind11==2.2.4' 'toml==0.10.0' 'pycryptodomex==3.7.3' --verbose
+    pip3 install --ignore-installed 'PyYAML==5.1.1'
+    pip3 install 'textX==1.7.1' 'graphviz==0.5.2' 'pydot==1.2.4' 'gitpython==2.1.11' 'pymultigen==0.2.0' 'Jinja2==2.10' --verbose
+    echo "installed pip3 packages"
 }
 
 #install apparmor_monkeys
 apparmor_monkeys_install(){
 
-	git clone https://github.com/RIAPS/apparmor_monkeys.git /tmp/3rdparty/apparmor_monkeys
-	cd /tmp/3rdparty/apparmor_monkeys
-	python3 setup.py install
-	sudo apt-get install apparmor-utils -y
-	rm -rf /tmp/3rdparty/apparmor_monkeys
-	echo "installed apparmor_monkeys"
+    git clone https://github.com/RIAPS/apparmor_monkeys.git /tmp/3rdparty/apparmor_monkeys
+    cd /tmp/3rdparty/apparmor_monkeys
+    python3 setup.py install
+    sudo apt-get install apparmor-utils -y
+    rm -rf /tmp/3rdparty/apparmor_monkeys
+    echo "installed apparmor_monkeys"
 }
 
 #install spdlog python logger
 spdlog_python_install(){
-	git clone https://github.com/RIAPS/spdlog-python.git /tmp/3rdparty/spdlog-python
-	cd /tmp/3rdparty/spdlog-python
-	git clone -b v0.17.0 --depth 1 https://github.com/gabime/spdlog.git
-	python3 setup.py install
-        rm -rf /tmp/3rdparty/spdlog-python
+    git clone https://github.com/RIAPS/spdlog-python.git /tmp/3rdparty/spdlog-python
+    cd /tmp/3rdparty/spdlog-python
+    git clone -b v0.17.0 --depth 1 https://github.com/gabime/spdlog.git
+    python3 setup.py install
+    rm -rf /tmp/3rdparty/spdlog-python
 	echo "installed spdlog python"
 }
 
@@ -212,53 +212,49 @@ zyre_czmq_prereq_install() {
 }
 
 pyzmq_install(){
-	cd /tmp/3rdparty
-	git clone https://github.com/zeromq/pyzmq.git
-	cd /tmp/3rdparty/pyzmq
-	git checkout v17.1.2
-	sudo python3 setup.py install
-        echo "installed pyzmq"
-        rm -rf /tmp/3rdparty/pyzmq
+    cd /tmp/3rdparty
+    git clone https://github.com/zeromq/pyzmq.git
+    cd /tmp/3rdparty/pyzmq
+    git checkout v17.1.2
+    sudo python3 setup.py install
+    echo "installed pyzmq"
+    rm -rf /tmp/3rdparty/pyzmq
 }
 
 #install bindings for czmq. Must be run after pyzmq, czmq install.
 czmq_pybindings_install(){
-
-	cd /tmp/3rdparty/czmq-amd64/bindings/python
-        sudo pip3 install . --verbose
-	echo "installed CZMQ pybindings"
+    cd /tmp/3rdparty/czmq-amd64/bindings/python
+    sudo pip3 install . --verbose
+    echo "installed CZMQ pybindings"
 }
 
 #install bindings for zyre. Must be run after zyre, pyzmq install.
 zyre_pybindings_install(){
-
-	cd /tmp/3rdparty/zyre-amd64/bindings/python
-        sudo pip3 install . --verbose
-	echo "installed Zyre pybindings"
-
+    cd /tmp/3rdparty/zyre-amd64/bindings/python
+    sudo pip3 install . --verbose
+    echo "installed Zyre pybindings"
 }
 
 #link pycapnp with installed library. Must be run after capnproto install.
 pycapnp_install() {
-
     CFLAGS=-I/usr/local/include LDFLAGS=-L/usr/local/lib pip3 install 'pycapnp==0.6.3'
     echo "linked pycapnp with capnproto"
 }
 
 # install gnutls
 gnutls_install(){
-	sudo apt-get install libgnutls30 -y
-	sudo apt-get install libgnutls30:armhf -y
-	sudo apt-get install libgnutls28-dev -y
-	echo "installed gnutls"
+    sudo apt-get install libgnutls30 -y
+    sudo apt-get install libgnutls30:armhf -y
+    sudo apt-get install libgnutls28-dev -y
+    echo "installed gnutls"
 }
 
 #install msgpack
 msgpack_install(){
-	sudo apt-get install libmsgpackc2:amd64 -y
-	sudo apt-get install libmsgpackc2:armhf -y
-	sudo apt-get install libmsgpack-dev:amd64 -y
-	echo "installed msgpack"
+    sudo apt-get install libmsgpackc2:amd64 -y
+    sudo apt-get install libmsgpackc2:armhf -y
+    sudo apt-get install libmsgpack-dev:amd64 -y
+    echo "installed msgpack"
 }
 
 #install opendht prerequisites
@@ -276,27 +272,26 @@ opendht_prereqs_install() {
 
 #install libsoc prerequisites
 libsoc_prereq_install(){
-	sudo apt-get install pkg-config -y
-	echo "installed libsoc prerequisites"
+    sudo apt-get install pkg-config -y
+    echo "installed libsoc prerequisites"
 }
 
 # install external packages using cmake
 # libraries installed: capnproto, lmdb, libnethogs, CZMQ, Zyre, opendht, libsoc
 externals_cmake_install(){
-	mkdir -p /home/riapsadmin/riaps-integration/riaps-x86runtime/build-amd64
-	cd /home/riapsadmin/riaps-integration/riaps-x86runtime/build-amd64
-        cmake -Darch=amd64 ..
-	make
-	cd /home/riapsadmin/riaps-integration/riaps-x86runtime
-	rm -rf /home/riapsadmin/riaps-integration/riaps-x86runtime/build-amd64
-	mkdir -p /home/riapsadmin/riaps-integration/riaps-x86runtime/build-armhf
-	cd /home/riapsadmin/riaps-integration/riaps-x86runtime/build-armhf
-	cmake -Darch=armhf ..
-        make
-        cd /home/riapsadmin/riaps-integration/riaps-x86runtime
-	rm -rf /home/riapsadmin/riaps-integration/riaps-x86runtime/build-armhf
-	echo "cmake install complete"
-
+    mkdir -p /home/riapsadmin/riaps-integration/riaps-x86runtime/build-amd64
+    cd /home/riapsadmin/riaps-integration/riaps-x86runtime/build-amd64
+    cmake -Darch=amd64 ..
+    make
+    cd /home/riapsadmin/riaps-integration/riaps-x86runtime
+    rm -rf /home/riapsadmin/riaps-integration/riaps-x86runtime/build-amd64
+    mkdir -p /home/riapsadmin/riaps-integration/riaps-x86runtime/build-armhf
+    cd /home/riapsadmin/riaps-integration/riaps-x86runtime/build-armhf
+    cmake -Darch=armhf ..
+    make
+    cd /home/riapsadmin/riaps-integration/riaps-x86runtime
+    rm -rf /home/riapsadmin/riaps-integration/riaps-x86runtime/build-armhf
+    echo "cmake install complete"
 }
 
 #eclipse install
@@ -327,7 +322,7 @@ eclipse_func() {
        sudo -H -u $1 chmod +x /home/$1/eclipse/eclipse
        eclipse_shortcut $1
     else
-	   echo "eclipse already installed at /home/$1/eclipse"
+       echo "eclipse already installed at /home/$1/eclipse"
     fi
 }
 
@@ -397,16 +392,16 @@ setup_ssh_keys () {
     sudo chown $1:$1 /home/$1/.ssh/authorized_keys
     sudo -H -u $1 chmod 600 /home/$1/.ssh/authorized_keys
     sudo -H -u $1 chmod 400 /home/$1/.ssh/id_rsa.key
-    #sudo -H -u $1 
+    #sudo -H -u $1
     echo "# RIAPS:  Add SSH keys to ssh agent on login" >> /home/$1/.bashrc
-    #sudo -H -u $1 
+    #sudo -H -u $1
     echo "ssh-add /home/$1/.ssh/id_rsa.key" >> /home/$1/.bashrc
 
     # Setup BBB ssh keys for use with VM
     sudo cp -r bbb_initial_keys /home/$1/.
     sudo chown $1:$1 -R /home/$1/bbb_initial_keys
     sudo -H -u $1 chmod 400 /home/$1/bbb_initial_keys/bbb_initial.key
-    #sudo -H -u $1 
+    #sudo -H -u $1
     echo "ssh-add /home/$1/bbb_initial_keys/bbb_initial.key" >> /home/$1/.bashrc
 
     # Transfer BBB rekeying script
