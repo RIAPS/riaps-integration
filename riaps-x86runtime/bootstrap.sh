4abc3d3dd9b1f4eb1b5e196f6b2fdb4bad90b15e
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2019-11-12 14:33:11 -0600

    Move pip3 install pydevd to version 1.8.0 - plan to move eclipse plugin to 7.4.0 version

--- riaps-x86runtime/bootstrap.sh
+++ riaps-x86runtime/bootstrap.sh
@@ -155,8 +155,7 @@ python_install () {
     sudo apt-get install python3-pip -y
     sudo apt-get install libpython3-dev:armhf -y
     sudo pip3 install --upgrade pip
-    sudo pip3 install pydevd
-    echo "installed python3 and pydev"
+    echo "installed python3"
 }
 
 curl_func () {
@@ -183,7 +182,7 @@ nethogs_prereq_install() {
 
 #install other required packages
 other_pip3_installs(){
-    pip3 install 'Adafruit_BBIO == 1.1.1' 'pydevd==1.4.0' 'rpyc==4.1.0' 'redis==2.10.6' 'hiredis == 0.2.0' 'netifaces==0.10.7' 'paramiko==2.6.0' 'cryptography==2.7' 'cgroups==0.1.0' 'cgroupspy==0.1.6' 'psutil==5.4.2' 'butter==0.12.6' 'lmdb==0.94' 'fabric3==1.14.post1' 'pyroute2==0.5.2' 'minimalmodbus==0.7' 'pyserial==3.4' 'pybind11==2.2.4' 'toml==0.10.0' 'pycryptodomex==3.7.3' --verbose
+    pip3 install 'Adafruit_BBIO == 1.1.1' 'pydevd==1.8.0' 'rpyc==4.1.0' 'redis==2.10.6' 'hiredis == 0.2.0' 'netifaces==0.10.7' 'paramiko==2.6.0' 'cryptography==2.7' 'cgroups==0.1.0' 'cgroupspy==0.1.6' 'psutil==5.4.2' 'butter==0.12.6' 'lmdb==0.94' 'fabric3==1.14.post1' 'pyroute2==0.5.2' 'minimalmodbus==0.7' 'pyserial==3.4' 'pybind11==2.2.4' 'toml==0.10.0' 'pycryptodomex==3.7.3' --verbose
     pip3 install --ignore-installed 'PyYAML==5.1.1'
     pip3 install 'textX==1.7.1' 'graphviz==0.5.2' 'pydot==1.2.4' 'gitpython==2.1.11' 'pymultigen==0.2.0' 'Jinja2==2.10' --verbose
     echo "installed pip3 packages"
