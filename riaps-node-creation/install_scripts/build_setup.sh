2389af870a2e5dc0368263d81d0ece62ae8b38d2
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2021-02-12 12:38:25 -0600

    Update install packages

--- riaps-node-creation/install_scripts/build_setup.sh
+++ riaps-node-creation/install_scripts/build_setup.sh
@@ -15,8 +15,7 @@ python_install() {
     sudo apt-get install python3-dev python3-setuptools -y
     sudo apt-get install python3-pip -y
     sudo pip3 install --upgrade pip
-    sudo pip3 install pydevd
-    echo ">>>>> installed python3 and pydev"
+    echo ">>>>> installed python3"
 }
 
 cython_install() {
